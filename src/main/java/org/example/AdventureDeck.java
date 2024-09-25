7f0f488b6358e82a08ea4b467df81ffc555f4e42
Gerry Huynh <gerry.huynh@gmail.com>
2024-09-24 23:37:56 -0400

    R-TEST-01: RESP-01 Game Sets Up Adventure Deck

---
+++ src/main/java/org/example/AdventureDeck.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AdventureDeck {
+    public int size() {
+        return 0;
+    }
+
+    public int getNumFoeCards() {
+        return 0;
+    }
+
+    public int getNumWeaponCards() {
+        return 0;
+    }
+
+    public Map<String, Integer> getFoeCards() {
+        return new HashMap<>();
+    }
+
+    public Map<String, Integer> getWeaponCards() {
+        return new HashMap<>();
+    }
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,142 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+  @Nested
+  @DisplayName("RESP_1: Adventure Deck Setup")
+  class RESP_1 {
+    private final AdventureDeck adventureDeck = new AdventureDeck();
+
+    @Test
+    @DisplayName("RESP_1_test_1: has 100 cards")
+    void RESP_1_test_1() {
+      assertEquals(100, adventureDeck.size(), "Total cards equal 100");
+    }
+
+    @Test
+    @DisplayName("RESP_1_test_2: has 100 cards with only Foe and Weapon cards")
+    void RESP_1_test_2() {
+      int totalFoeAndWeaponCards = adventureDeck.getNumFoeCards() + adventureDeck.getNumWeaponCards();
+      assertEquals(100, totalFoeAndWeaponCards, "Foe + Weapon cards equals 100 cards");
+    }
+
+    @Nested
+    @DisplayName("RESP_1_test_3: Foe Cards")
+    class RESP_1_test_3 {
+      @Test
+      @DisplayName("RESP_1_test_3_1: has 50 cards")
+      void RESP_1_test_3_1() {
+        assertEquals(50, adventureDeck.getNumFoeCards());
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_3_2: has 8 F5 cards")
+      void RESP_1_test_3_2() {
+        assertEquals(8, adventureDeck.getFoeCards().get("F5"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_3_3: has 7 F10 cards")
+      void RESP_1_test_3_3() {
+        assertEquals(7, adventureDeck.getFoeCards().get("F10"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_3_4: has 8 F15 cards")
+      void RESP_1_test_3_4() {
+        assertEquals(8, adventureDeck.getFoeCards().get("F15"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_3_5: has 7 F20 cards")
+      void RESP_1_test_3_5() {
+        assertEquals(7, adventureDeck.getFoeCards().get("F20"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_3_6: has 7 F25 cards")
+      void RESP_1_test_3_6() {
+        assertEquals(7, adventureDeck.getFoeCards().get("F25"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_3_7: has 4 F30 cards")
+      void RESP_1_test_3_7() {
+        assertEquals(4, adventureDeck.getFoeCards().get("F30"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_3_8: has 4 F35 cards")
+      void RESP_1_test_3_8() {
+        assertEquals(4, adventureDeck.getFoeCards().get("F35"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_3_9: has 2 F40 cards")
+      void RESP_1_test_3_9() {
+        assertEquals(2, adventureDeck.getFoeCards().get("F40"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_3_10: has 2 F50 cards")
+      void RESP_1_test_3_10() {
+        assertEquals(2, adventureDeck.getFoeCards().get("F50"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_3_11: has 1 F70 card")
+      void RESP_1_test_3_11() {
+        assertEquals(1, adventureDeck.getFoeCards().get("F70"));
+      }
+    }
+
+    @Nested
+    @DisplayName("RESP_1_test_4: Weapon Cards")
+    class RESP_1_test_4 {
+      @Test
+      @DisplayName("RESP_1_test_4_1: has 50 cards")
+      void RESP_1_test_4_1() {
+        assertEquals(50, adventureDeck.getNumWeaponCards());
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_4_2: has 6 D5 cards")
+      void RESP_1_test_4_2() {
+        assertEquals(6, adventureDeck.getWeaponCards().get("D5"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_4_3: has 12 H10 cards")
+      void RESP_1_test_4_3() {
+        assertEquals(12, adventureDeck.getWeaponCards().get("H10"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_4_4: has 16 S10 cards")
+      void RESP_1_test_4_4() {
+        assertEquals(16, adventureDeck.getWeaponCards().get("S10"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_4_5: has 8 B15 cards")
+      void RESP_1_test_4_5() {
+        assertEquals(8, adventureDeck.getWeaponCards().get("B15"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_4_6: has 6 L20 cards")
+      void RESP_1_test_4_6() {
+        assertEquals(6, adventureDeck.getWeaponCards().get("L20"));
+      }
+
+      @Test
+      @DisplayName("RESP_1_test_4_7: has 2 E30 cards")
+      void RESP_1_test_4_7() {
+        assertEquals(2, adventureDeck.getWeaponCards().get("E30"));
+      }
+    }
+  }
+}
