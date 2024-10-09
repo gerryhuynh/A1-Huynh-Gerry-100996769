f0e633c834d86db973d5a4d5fed0b659ea7323ac
Gerry Huynh <gerry.huynh@gmail.com>
2024-10-09 16:57:06 -0400

    R-TEST-05: RESP-05 Game Distributes Adventure Cards to Players

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -3,6 +3,8 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.example.decks.AdventureDeck;
+
 public class Game {
   protected static final int MAX_PLAYERS = 4;
 
@@ -16,6 +18,14 @@ public class Game {
     for (int i = 1; i <= MAX_PLAYERS; i++) {
       players.add(new Player("P" + i));
     }
+  }
+
+  public void dealAdventureCards() {
+    return;
+  }
+
+  public AdventureDeck getAdventureDeck() {
+    return new AdventureDeck();
   }
 
   public int getNumPlayers() {
